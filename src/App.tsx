5258fcac85fb6eea221c93f4cf1d5bc28cac1eee
25LUKE <EricLuke25@gmail.com>
2023-03-19 16:27:07 +0100

    ShoppingList

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Header from "./component/Header"
+import Footer from "./component/Footer"
+import Cart from "./component/Cart"
+import ProductList from "./component/ProductList"
+import { useState } from "react"
+
+
+function App() {
+  const [viewCart, setViewCart] = useState<boolean>(false)
+
+  const pageContent = viewCart ? <Cart /> : <ProductList />
+
+  const content = (
+    <>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />{pageContent}
+      <Footer viewCart={viewCart} />
+    </>
+  )
+  return content
+}
+
+export default App
